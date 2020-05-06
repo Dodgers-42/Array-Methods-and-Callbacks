50fb2629bd166fdcf187576bb3c2211b7a9be7ee
Dodger-42 <kevin.bing.barber@gmail.com>
2020-05-05 18:46:49 -0700

    first

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ console.log(fifaData);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
+function getFinals(/* data/) {
+return
     /* code here */
 
 };
